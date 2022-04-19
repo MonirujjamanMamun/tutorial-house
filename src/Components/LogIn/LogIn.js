e7ed272dbd27e0124cf61cfe5964bfcab4b56712
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-04-19 02:50:51 -0700

    register added

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 import './LogIn.css';
 
 const LogIn = () => {
@@ -17,17 +18,23 @@ const LogIn = () => {
         <p> Or </p>
         <div className='horizental-line'> </div>
       </div>
-      <Form>
+      <Form className='w-50 mx-auto'>
         <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Control type='email' placeholder='Enter email' required />
         </Form.Group>
         <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Control type='password' placeholder='Password' required />
         </Form.Group>
+        <p>Forgot Password?</p>
         <Button variant='primary w-50 mx-auto d-block mb-2' type='submit'>
           Log In
         </Button>
       </Form>
+        <div className='w-50 mx-auto'><hr></hr></div>
+      <div className='d-flex justify-content-between w-50 mx-auto'>
+        <p>Don't have an account?</p>
+        <Link className='text-decoration-none' to='/register'>Register Now</Link >
+      </div>
     </div>
   )
 }
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,9 +1,36 @@
+
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import FormCheckInput from 'react-bootstrap/esm/FormCheckInput';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h1>This is register</h1>
+        <div className='container w-50 mx-auto h-100'>
+           <h1 className='text-center my-3'>Register</h1>
+            <Form className='w-50 mx-auto'>
+                <Form.Group className='mb-3' controlId='formBasicName'>
+                    <Form.Control type='text' placeholder='Enter Name'/>
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                    <Form.Control type='email' placeholder='Enter email' required />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                    <Form.Control type='password' placeholder='Password' required />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                    <Form.Control type='password' placeholder='Confirm-Password' required />
+                </Form.Group>
+                <FormCheckInput></FormCheckInput> <span>I agre all terms & condition</span>
+                <Button variant='primary w-50 mx-auto d-block mb-2' type='submit'>
+                    Register
+                </Button>
+            </Form>
+            <div className='w-50 mx-auto'><hr></hr></div>
+            <div className='d-flex justify-content-between w-50 mx-auto'>
+                <p>Have an account?</p>
+                <Link className='text-decoration-none' to='/login'>LogIn Here</Link >
+            </div>
         </div>
     );
 };

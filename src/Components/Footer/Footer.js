7c5e3e0550dde8decdf49faf0d0e5ec454699132
Monirujjaman Mamun <96871498+MonirujjamanMamun@users.noreply.github.com>
2022-04-19 02:14:51 -0700

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer</h1>
+        <div className='bg-dark text-center p-3 my-3'>
+            <h6 className='text-light cursor-pointer'> Info <span className='pink-color'>|</span> Support <span className='pink-color'>|</span> Marketing <span className='pink-color'>|</span> </h6>
+            <h6 className='text-light'>Terms of Us <span className='pink-color'>|</span> Privacy Policy <span className='pink-color'>|</span></h6>
+            <p className='text-light'><span>&copy;</span> <span>2022 Tutorial House</span></p>
         </div>
     );
 };
--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React from 'react'
+import { Button, Form } from 'react-bootstrap'
+import './LogIn.css';
 
 const LogIn = () => {
-    return (
+  return (
+    <div className='container w-50 mx-auto'>
+      <h2 className='text-primary text-center mt-2'> Log In </h2>
+      <Button variant='primary w-50 mx-auto d-block mb-2' type='submit'>
+        Continue With Google
+      </Button>
+      <Button variant='primary w-50 mx-auto d-block mb-2' type='submit'>
+        Continue With GitHub
+      </Button>
+      <div className='or-section'>
+        <div className='horizental-line'> </div>
+        <p> Or </p>
+        <div className='horizental-line'> </div>
+      </div>
+      <Form>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Control type='email' placeholder='Enter email' required />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
+          <Form.Control type='password' placeholder='Password' required />
+        </Form.Group>
+        <Button variant='primary w-50 mx-auto d-block mb-2' type='submit'>
+          Log In
+        </Button>
+      </Form>
+    </div>
+  )
+}
 
-        <div className='container w-50 mx-auto'>
-            <h2 className='text-primary text-center mt-2'>Log In</h2>
-            <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
-                Continue With Google
-            </Button>
-            <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
-                Continue With GitHub
-            </Button>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" required />
-                </Form.Group>
-                <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
-                    Log In
-                </Button>
-            </Form>
-        </div>
-
-    );
-};
-
-export default LogIn;
+export default LogIn
